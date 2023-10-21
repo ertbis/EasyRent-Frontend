43d9c09f153d66d3ebad31ec57828d7e4ae9bdb0
Alabi Damilola Daniel <eminencemailx@gmail.com>
2023-10-21 09:20:16 -0700

    added useProtecteed custom hook to protect the route

--- src/app/(auth)/unauthorized/page.tsx
+++ src/app/(auth)/unauthorized/page.tsx
@@ -3,8 +3,8 @@
 const Unauthorized = () => {
     return ( 
         <div className="bg-[#ff0000] flex flex-col justify-center items-center text-white h-screen w-full">
-            <h1 className="text-white text-[3rem]">UnAuthorized</h1>
-            <h1 className="text-white">AUSTIN Has not designed the unauthorized page</h1>
+            <h1 className="text-center text-white text-[3rem]">UnAuthorized</h1>
+            <h1 className="text-center text-white">AUSTIN Has not designed the unauthorized page</h1>
         </div>
      );
 }

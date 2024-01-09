4dbe6a1023be68ad254197de5b1e06febcd4eeb4
eminence <eminencemailx@gmail.com>
2024-01-09 15:03:26 +0000

    fix connect chat issuee

--- src/app/paymentsuccess/page.tsx
+++ src/app/paymentsuccess/page.tsx
@@ -16,7 +16,7 @@ const SuccessPage = () => {
         try {
             const resp = await  createChats()
             console.log(resp)
-           window.location.replace(`/chatagent/${resp.data._id}`);
+           window.location.replace(`/chatagent/${resp.data[0]._id}`);
 
         } catch (e : any) {
             setErrorModal(true)
